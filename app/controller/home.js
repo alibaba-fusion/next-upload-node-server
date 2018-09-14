8eb0ba0a9677c0c86efac7bdb0f5e4928958a982
muchen <tao360907796@gmail.com>
2018-09-15 00:31:35 +0800

    feat: add index.html render

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,9 +1,21 @@
 'use strict';
-
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
 const Controller = require('egg').Controller;
+const readFile = promisify(fs.readFile);
+
 
 class HomeController extends Controller {
   async index() {
+    const {ctx} = this;
+    const {baseDir} = ctx.app;
+    const filePath = path.join(baseDir, '/app/view/index.html');
+    const fileContent = await readFile(filePath, 'utf8');
+    this.ctx.body = fileContent;
+  }
+
+  async fileUpload() {
     this.ctx.body = 'hi, egg';
   }
 }
